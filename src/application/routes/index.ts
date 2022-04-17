e23d87ea932862acc80b1152f86f526b88ba5e5b
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-04-17 14:14:43 +0200

    removing bad lib

--- src/application/routes/index.ts
+++ src/application/routes/index.ts
@@ -1,12 +1,11 @@
 import path from 'path';
-import fg from 'fast-glob';
+import glob from 'glob';
 import { Router } from 'express';
 
 export function registerRoutes(router: Router) {
-  fg.sync('./**/*.route.ts', { onlyFiles: false }).forEach(dir => {
+  glob.sync('src/application/routes/**/*.route.ts').forEach(dir => {
     const modulePath = path.resolve(__dirname + '../../../../', dir);
     const route = require(modulePath);
     route.register(router);
-    return dir;
   });
 }
